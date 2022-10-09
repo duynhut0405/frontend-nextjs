1fe8c7f21e384a34e88f2d167725c5ddc07a0959
duynhut0405 <57499051+duynhut0405@users.noreply.github.com>
2022-10-09 17:04:05 +0700

    add folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
+import Image from 'next/future/image'
+import logo from '@images/logo.jpg'
 export default function Home() {
+  console.log('🚀 ~ file: index.js ~ line 9 ~ Home ~ logo', logo)
   return (
     <div>
       <p className='text-red-700 font-bold'>TEST</p>
+      <Image src={logo} />
     </div>
   )
 }
