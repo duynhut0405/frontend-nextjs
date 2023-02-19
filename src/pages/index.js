8f98d374577b318a6c243094d67b141985804d36
duynhut0405 <57499051+duynhut0405@users.noreply.github.com>
2023-02-20 00:11:34 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
-import { BlockHeroContent } from '@components/blocks'
+import {
+  BlockAboutUs,
+  BlockFanfact,
+  BlockHeroContent,
+  BlockOurProject,
+  BlockOurSolution,
+  BlockServices,
+  BlockTestimonials,
+} from '@components/blocks'
 import dynamic from 'next/dynamic'
 import { Fragment } from 'react'
 const BackgroundElement = dynamic(import('../components/layout/BackgroundElement'))
-const BlockAboutUs = dynamic(import('../components/blocks/BlockAboutUs'))
-const BlockFanfact = dynamic(import('../components/blocks/BlockFanfact'))
-const BlockOurSolution = dynamic(import('../components/blocks/BlockOurSolution'))
-const BlockOurProject = dynamic(import('../components/blocks/BlockOurProject'))
-const BlockServices = dynamic(import('../components/blocks/BlockServices'))
-const BlockTestimonials = dynamic(import('../components/blocks/BlockTestimonials'))
+
 export default function Home() {
   return (
     <Fragment>
