3eb1ad19bd7eae09722af79d66d749f2176297e0
duynhut0405 <57499051+duynhut0405@users.noreply.github.com>
2023-03-01 00:07:25 +0700

    animation

--- src/components/blocks/BlockHeroContent/index.js
+++ src/components/blocks/BlockHeroContent/index.js
@@ -6,77 +6,107 @@ import Wire from '@images/png/Image1.png'
 import Wire2 from '@images/png/Image4.png'
 import WorldClass from '@images/png/Image2.png'
 import { OrnamentCicle, OrnamentStar, PlayVideo } from '@images/svg'
+import { motion } from 'framer-motion'
+import { FADE_DOWN_ANIMATION_VARIANTS } from '@data/constants'
 export default function BlockHeroContent() {
   return (
-    <Container className='relative' type='large'>
-      <div className='absolute bottom-0 lg:top-32'>
-        <OrnamentStar className='w-6 h-6 lg:w-14 lg:h-14' />
-      </div>
-      <div className='absolute -top-10 right-12 lg:top-0 lg:right-44'>
-        <OrnamentStar className='w-6 h-6 lg:w-14 lg:h-14' />
-      </div>
-      <div className='hidden lg:block absolute right-0 -z-10'>
-        <Image src={Wire} alt='img' priority={100} quality={75} />
-      </div>
-      <div className='block lg:hidden absolute right-4 -z-10'>
-        <Image src={Wire2} alt='img' priority={100} quality={75} />
-      </div>
-      <div className='flex justify-center items-center flex-col'>
-        <div className='relative'>
-          <div className='absolute top-8 -left-10 lg:top-20 lg:-left-56 w-[130px] h-[80px] lg:w-[240px] lg:h-auto'>
-            <Image
-              src={WorldClass}
-              alt='img'
-              priority={true}
-              quality={75}
-              className='-rotate-[25deg] w-[95px] lg:w-auto'
-            />
-          </div>
-          <h1 className='font-bold ml-14 lg:ml-0 text-[70px] md:text-[100px] lg:text-[10rem] mb-4 lg:mb-0 leading-none'>
-            Digital
-          </h1>
-        </div>
-        <div className='flex flex-col lg:flex-row justify-between gap-10 lg:gap-0'>
-          <div className='w-[250px] h-[300px] relative mr-20 self-end mt-[25%] order-2 lg:order-1'>
-            <Image
-              src={Image2}
-              fill
-              alt='img'
-              priority={true}
-              quality={50}
-              className='rounded-[30px] object-cover'
-            />
-            <div className='absolute -bottom-[25%] -right-[25%] text-white -z-10'>
-              <OrnamentCicle color={'white'} />
-            </div>
-          </div>
-
-          <div className='block lg:flex order-1 lg:order-2 gap-2'>
-            <div>
-              <h1 className='font-bold text-7xl md:text-[100px] lg:text-[150px] leading-none'>
-                Agency
-              </h1>
-              <p className='text-2xl leading-10 max-w-[500px] font-normal text-CARD mt-6 lg:mt-10'>
-                Full-service design support that growing B2B companies need. World leading agency.
-              </p>
-              <Button className='mt-6 lg:mt-14'>Our Projects</Button>
-            </div>
-            <div className='w-full lg:w-[466px] h-[333px] lg:h-[475px] relative mt-14'>
+    <motion.div
+      initial='hidden'
+      whileInView='show'
+      animate='show'
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}>
+      <Container className='relative' type='large'>
+        <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS} className='absolute bottom-0 lg:top-32'>
+          <OrnamentStar className='w-6 h-6 lg:w-14 lg:h-14' />
+        </motion.div>
+        <motion.div
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          className='absolute -top-10 right-12 lg:top-0 lg:right-44'>
+          <OrnamentStar className='w-6 h-6 lg:w-14 lg:h-14' />
+        </motion.div>
+        <motion.div
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          className='hidden lg:block absolute right-0 -z-10'>
+          <Image src={Wire} alt='img' priority={100} quality={75} />
+        </motion.div>
+        <motion.div
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          className='block lg:hidden absolute right-4 -z-10'>
+          <Image src={Wire2} alt='img' priority={100} quality={75} />
+        </motion.div>
+        <div className='flex justify-center items-center flex-col'>
+          <div className='relative'>
+            <div className='absolute top-8 -left-10 lg:top-20 lg:-left-56 w-[130px] h-[80px] lg:w-[240px] lg:h-auto'>
               <Image
-                src={Image1}
+                src={WorldClass}
+                alt='img'
+                priority={true}
+                quality={75}
+                className='-rotate-[25deg] w-[95px] lg:w-auto'
+              />
+            </div>
+            <h1 className='font-bold ml-14 lg:ml-0 text-[70px] md:text-[100px] lg:text-[10rem] mb-4 lg:mb-0 leading-none'>
+              Digital
+            </h1>
+          </div>
+          <div className='flex flex-col lg:flex-row justify-between gap-10 lg:gap-0'>
+            <motion.div
+              variants={FADE_DOWN_ANIMATION_VARIANTS}
+              className='w-[250px] h-[300px] relative mr-20 self-end mt-[25%] order-2 lg:order-1'>
+              <Image
+                src={Image2}
                 fill
                 alt='img'
                 priority={true}
-                quality={100}
-                className='rounded-[50px] object-cover'
+                quality={50}
+                className='rounded-[30px] object-cover'
               />
-              <div className='absolute -bottom-20 right-10'>
-                <PlayVideo />
+              <div className='absolute -bottom-[25%] -right-[25%] text-white -z-10'>
+                <OrnamentCicle color={'white'} />
+              </div>
+            </motion.div>
+
+            <div className='block lg:flex order-1 lg:order-2 gap-2'>
+              <div>
+                <h1 className='font-bold text-7xl md:text-[100px] lg:text-[150px] leading-none'>
+                  Agency
+                </h1>
+                <motion.p
+                  variants={FADE_DOWN_ANIMATION_VARIANTS}
+                  className='text-2xl leading-10 max-w-[500px] font-normal text-CARD mt-6 lg:mt-10'>
+                  Full-service design support that growing B2B companies need. World leading agency.
+                </motion.p>
+                <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
+                  <Button className='mt-6 lg:mt-14'>Our Projects</Button>
+                </motion.div>
+              </div>
+              <div className='w-full lg:w-[466px] h-[333px] lg:h-[475px] relative mt-14'>
+                <Image
+                  src={Image1}
+                  fill
+                  alt='img'
+                  priority={true}
+                  quality={100}
+                  className='rounded-[50px] object-cover'
+                />
+                <motion.div
+                  variants={FADE_DOWN_ANIMATION_VARIANTS}
+                  className='absolute -bottom-20 right-10'>
+                  <PlayVideo />
+                </motion.div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </motion.div>
   )
 }
